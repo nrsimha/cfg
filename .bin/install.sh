04e033636bc2fe42ef28bbc1d245fe3ee88f96b5
Nrsimha <petr.vacha@gmail.com>
2021-10-14 13:23:18 +0200

    Changed matching of the files, so it moves existing files in home directory.

--- .bin/install.sh
+++ .bin/install.sh
@@ -7,8 +7,8 @@ config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    echo "Backing up pre-existing dot files which are not in subdirectories.";
+    config checkout 2>&1 | egrep "$(printf '\t').*" | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
